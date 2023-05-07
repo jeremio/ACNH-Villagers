199f4511e2388a3ef882fff8dcbc50befd87596c
cotiniaux <jcotiniaux@gmail.com>
2023-05-07 17:46:00 +0200

    chore(*): revert popover

--- src/components/Mosaique.vue
+++ src/components/Mosaique.vue
@@ -2,49 +2,47 @@
   <div class="mosaique">
     <NImageGroup :theme-overrides="imageGroupThemeOverrides">
       <NSpace>
-        <!--        <template > -->
-        <!--          <NPopover trigger="hover" raw> -->
-        <!--            <template #trigger> -->
-        <NImage
-          v-for="villager in props.data" :key="villager"
-          :lazy="true"
-          :src="image(villager)" width="100" height="100"
-          :alt="name(villager)"
-          :title="name(villager)"
-        />
-        <!--                <template #placeholder> -->
-        <!--                  <div -->
-        <!--                    style=" -->
-        <!--            width: 100px; -->
-        <!--            height: 100px; -->
-        <!--            display: flex; -->
-        <!--            align-items: center; -->
-        <!--            justify-content: center; -->
-        <!--            background-color: #8882; -->
-        <!--          " -->
-        <!--                  > -->
-        <!--                    {{ name(villager) }} -->
-        <!--                  </div> -->
-        <!--                </template> -->
-        <!--              </NImage> -->
-        <!--            </template> -->
-        <!--            <div -->
-        <!--              style=" -->
-        <!--                  width: 175px; -->
-        <!--                  padding: 10px; -->
-        <!--                  background-color: #3c817f; -->
-        <!--                  transform-origin: inherit; -->
-        <!--            " -->
-        <!--            > -->
-        <!--              <div> {{ name(villager) }}</div> -->
-        <!--              <div> {{ $t('filters.gender') }} : {{ $t(`genders.${gender(villager)}`) }}</div> -->
-        <!--              <div> {{ $t('filters.hobby') }} : {{ $t(`hobbies.${hobby(villager)}`) }}</div> -->
-        <!--              <div> {{ $t('filters.personality') }} : {{ $t(`personality.${personality(villager)}`) }}</div> -->
-        <!--              <div> {{ $t('filters.specie') }} : {{ $t(`species.${species(villager)}`) }}</div> -->
-        <!--              <div> {{ $t('filters.birthday') }} : {{ birthdayString(villager) }}</div> -->
-        <!--            </div> -->
-        <!--          </NPopover> -->
-        <!--        </template> -->
+        <template v-for="villager in props.data" :key="villager">
+          <NPopover trigger="hover" raw>
+            <template #trigger>
+              <NImage
+                lazy :src="image(villager)" width="100" height="100"
+                :alt="name(villager)"
+                :show-toolbar-tooltip="true"
+              >
+                <template #placeholder>
+                  <div
+                    style="
+            width: 100px;
+            height: 100px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #8882;
+          "
+                  >
+                    {{ name(villager) }}
+                  </div>
+                </template>
+              </NImage>
+            </template>
+            <div
+              style="
+                  width: 175px;
+                  padding: 10px;
+                  background-color: #3c817f;
+                  transform-origin: inherit;
+            "
+            >
+              <div> {{ name(villager) }}</div>
+              <div> {{ $t('filters.gender') }} : {{ $t(`genders.${gender(villager)}`) }}</div>
+              <div> {{ $t('filters.hobby') }} : {{ $t(`hobbies.${hobby(villager)}`) }}</div>
+              <div> {{ $t('filters.personality') }} : {{ $t(`personality.${personality(villager)}`) }}</div>
+              <div> {{ $t('filters.specie') }} : {{ $t(`species.${species(villager)}`) }}</div>
+              <div> {{ $t('filters.birthday') }} : {{ birthdayString(villager) }}</div>
+            </div>
+          </NPopover>
+        </template>
       </NSpace>
     </NImageGroup>
   </div>
@@ -53,9 +51,8 @@
 <script setup>
 import _get from 'lodash/get'
 import { useI18n } from 'vue-i18n'
-
 import { computed } from 'vue'
-import { NImage, NImageGroup, NSpace, useThemeVars } from 'naive-ui'
+import { NImage, NImageGroup, NPopover, NSpace, useThemeVars } from 'naive-ui'
 
 const props = defineProps({
   data: {
@@ -89,23 +86,23 @@ const imageGroupThemeOverrides = computed(() => {
   return themeOverrides
 })
 
-// const hobby = (villager) => {
-//   return _get(villager, 'hobby', null)
-// }
-//
-// const gender = (villager) => {
-//   return _get(villager, 'gender', null)
-// }
-//
-// const birthdayString = (villager) => {
-//   return _get(villager, 'birthday-string', null)
-// }
-//
-// const species = (villager) => {
-//   return _get(villager, 'species', null)
-// }
-//
-// const personality = (villager) => {
-//   return _get(villager, 'personality', null)
-// }
+function hobby(villager) {
+  return _get(villager, 'hobby', null)
+}
+
+function gender(villager) {
+  return _get(villager, 'gender', null)
+}
+
+function birthdayString(villager) {
+  return _get(villager, 'birthday-string', null)
+}
+
+function species(villager) {
+  return _get(villager, 'species', null)
+}
+
+function personality(villager) {
+  return _get(villager, 'personality', null)
+}
 </script>
