e22be3e8fe21a21c7358b0a0d3c94522803cca8b
JereMiO <jcotiniaux@gmail.com>
2024-10-06 18:51:59 +0200

    not use $i18n

--- src/components/menu/lang/Lang.vue
+++ src/components/menu/lang/Lang.vue
@@ -1,11 +1,15 @@
 <template>
   <NSpace vertical>
-    <NSelect v-model:value="$i18n.locale" :options="options" :consistent-menu-width="false" />
+    <NSelect v-model:value="locale" :options="options" :consistent-menu-width="false" />
   </NSpace>
 </template>
 
 <script setup lang="ts">
 import { NSelect, NSpace } from 'naive-ui'
+
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
 
 const options = [
   { value: 'fr', label: 'FR' },
